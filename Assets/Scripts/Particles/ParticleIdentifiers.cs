18dd27405d1dcb6c11f0f2a7bf9712c0d7c43836
Nicolas Tanda <ntanda.dctmedia@gmail.com>
2020-11-08 12:54:23 +0000

    Added particles when linking a match

--- Assets/Scripts/Particles/ParticleIdentifiers.cs
+++ Assets/Scripts/Particles/ParticleIdentifiers.cs
@@ -12,18 +12,9 @@ namespace MAGTask
         /// 
         public static readonly string k_textReward = "Particles/TextReward";
         public static readonly string k_currencyReward = "Particles/CurrencyReward";
-        public static readonly string k_starburst = "Particles/Starburst";
-        public static readonly string k_reveal = "Particles/StarburstBright";
-        public static readonly string k_confetti = "Particles/Confetti";
-        public static readonly string k_purchase = "Particles/Purchase";
-        public static readonly string k_coinLost = "Particles/CoinLost";
-        public static readonly string k_fireworks = "Particles/Fireworks";
-        public static readonly string k_loveburst = "Effects/Loveburst";
-        public static readonly string k_loveburstPet = "Effects/LoveburstPet";
 
-        /// Digging game
+        /// Linking game
         /// 
-        public static readonly string k_tileRevealed = "Particles/TileRevealed";
-        public static readonly string k_tileMine = "Particles/TileMine";
+        public static readonly string k_tilePop = "Particles/TilePop";
     }
 }
